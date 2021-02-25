db4dc0e5d9aedf21df9530b26acfe40d05d11ad7
steve brazier <steve.brazier@trioptima.com>
2021-02-25 11:08:50 +0100

    add blog speed to the footer.html

--- javascripts/blogspeed.js
+++ javascripts/blogspeed.js
@@ -7,12 +7,15 @@
 {% assign oldest = allposts | last %}
 
 
-let oldest = Date.parse("{{oldest.date}}".replace(" +0000", ""));
-let n5post = Date.parse("{{allposts[4].date}}".replace(" +0000", ""));
-let count = {{site.posts.size}};
+const oldest = Date.parse("{{oldest.date}}".replace(" +0000", ""));
+const n5post = Date.parse("{{allposts[4].date}}".replace(" +0000", ""));
+const count = {{site.posts.size}};
 
 // Blog posts per year
-let lifetimeSpeed = (count / ((Date.now() - oldest) / 1000 / 60 / 60 / 24 / 365.25)).toFixed(2);
-let recentSpeed =  (5 / ((Date.now() - n5post) / 1000 / 60 / 60 / 24 / 365.25)).toFixed(2);
+const lifetimeSpeed = (count / ((Date.now() - oldest) / 1000 / 60 / 60 / 24 / 365.25)).toFixed(2);
+const recentSpeed =  (5 / ((Date.now() - n5post) / 1000 / 60 / 60 / 24 / 365.25)).toFixed(2);
 
 console.log(`Current blog speed is ${recentSpeed} posts per year (The lifetime average is ${lifetimeSpeed})`);
+
+const speedSection = document.getElementById("blogspeed");
+speedSection.innerHTML = (`<p>Currently blogging at ${recentSpeed} posts per year</p>`)
