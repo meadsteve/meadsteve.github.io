87b922d302108c2c0340ad21bfd1d7bbc485255b
steve brazier <steve.brazier@trioptima.com>
2021-02-26 08:05:38 +0100

    bring back the decimal point

--- javascripts/blogspeed.js
+++ javascripts/blogspeed.js
@@ -17,18 +17,19 @@ const oldest = postDates[0];
 const n5post = postDates[postDates.length - 5];
 const count = postDates.length;
 const threeishMonths = 1000 * 60 * 60 * 24 * 30 * 3;
+const oneYear = 1000 * 60 * 60 * 24 * 365.25;
 
 
 // Returns the posts per year for the last 3 months
-function rollingAverage(atDate) {
-    const threeishMonthsAgo = atDate - threeishMonths;
-    const postsInThePeriod = postDates.filter(d => d >= threeishMonthsAgo);
-    return postsInThePeriod.length * 4;
+function recentAverage(atDate, samplePeriod) {
+    const cutOff = atDate - samplePeriod;
+    const postsInThePeriod = postDates.filter(d => d >= cutOff);
+    return (postsInThePeriod.length * (oneYear / samplePeriod)).toFixed(1);
 }
 
 // Blog posts per year
-const lifetimeSpeed = (count / ((Date.now() - oldest) / 1000 / 60 / 60 / 24 / 365.25)).toFixed(2);
-const recentSpeed =  rollingAverage(Date.now());
+const lifetimeSpeed = (count / ((Date.now() - oldest) / 1000 / 60 / 60 / 24 / 365.25)).toFixed(1);
+const recentSpeed =  recentAverage(Date.now(), threeishMonths);
 
 console.log(`Current blog speed is ${recentSpeed} posts per year (The lifetime average is ${lifetimeSpeed})`);
 const speedSection = document.getElementById("blogspeed");
