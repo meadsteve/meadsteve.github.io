469e4dc3190acf1ce1a82984895144701b3ed6fe
steve.brazier <steve.brazier@epidemicsound.com>
2024-10-08 13:19:33 +0200

    Fix local previewing on an m2

--- watch.sh
+++ watch.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-docker build -t githubpage-preview .
-docker run -p 4000:4000 -v $(pwd):/site githubpage-preview
+docker build --platform linux/arm64 -t githubpage-preview .
+docker run --platform linux/arm64 -p 4000:4000 -v $(pwd):/site githubpage-preview
